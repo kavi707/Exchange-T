fb6ad858f22a7b77293cc7ff378737fba20e8fa0
Kavimal Wijewardana <kavi707@gmail.com>
2017-09-10 10:04:52 +0530

    Remove application title from the action bar

--- app/src/main/java/com/kavi/droid/exchange/activities/LandingActivity.java
+++ app/src/main/java/com/kavi/droid/exchange/activities/LandingActivity.java
@@ -46,6 +46,9 @@ public class LandingActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        // Remove app title from the action bar
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
     @Override
@@ -56,28 +59,6 @@ public class LandingActivity extends AppCompatActivity
         } else {
             super.onBackPressed();
         }
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.landing, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
     }
 
     @SuppressWarnings("StatementWithEmptyBody")
