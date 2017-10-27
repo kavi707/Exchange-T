3708e2568301f0c58ff027da1d4a083f166b115c
Kavimal Wijewardana <kavi707@gmail.com>
2017-10-26 23:23:47 -0400

    Start to create ticket request details screen

--- app/src/main/java/com/kavi/droid/exchange/activities/TicketRequestDetailActivity.java
+++ app/src/main/java/com/kavi/droid/exchange/activities/TicketRequestDetailActivity.java
@@ -2,6 +2,8 @@ package com.kavi.droid.exchange.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.kavi.droid.exchange.R;
 
@@ -12,6 +14,16 @@ import com.kavi.droid.exchange.R;
 
 public class TicketRequestDetailActivity extends ExchangeBaseActivity {
 
+    private TextView requestedNameTextView;
+    private ImageView reqUserImageView;
+    private TextView reqTypeTextView;
+    private TextView qtyTextView;
+    private TextView startToEndTextView;
+    private TextView ticketDateTextView;
+    private TextView ticketTimeTextView;
+    private TextView contactNumberTextView;
+    private TextView emailTextView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,6 +33,14 @@ public class TicketRequestDetailActivity extends ExchangeBaseActivity {
     }
 
     private void setUpViews() {
-
+        requestedNameTextView = (TextView) findViewById(R.id.requestedNameTextView);
+        reqUserImageView = (ImageView) findViewById(R.id.reqUserImageView);
+        reqTypeTextView = (TextView) findViewById(R.id.reqTypeTextView);
+        qtyTextView = (TextView) findViewById(R.id.qtyTextView);
+        startToEndTextView = (TextView) findViewById(R.id.startToEndTextView);
+        ticketDateTextView = (TextView) findViewById(R.id.ticketDateTextView);
+        ticketTimeTextView = (TextView) findViewById(R.id.ticketTimeTextView);
+        contactNumberTextView = (TextView) findViewById(R.id.contactNumberTextView);
+        emailTextView = (TextView) findViewById(R.id.emailTextView);
     }
 }
