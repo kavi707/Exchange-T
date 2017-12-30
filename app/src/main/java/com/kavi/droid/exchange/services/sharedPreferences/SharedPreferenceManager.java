b87662484010aee1e1387c743e342d5a338078c7
Kavimal Wijewardana <kavi707@gmail.com>
2017-12-29 19:21:13 -0500

    Update share preference manager to persists objects

--- app/src/main/java/com/kavi/droid/exchange/services/sharedPreferences/SharedPreferenceManager.java
+++ app/src/main/java/com/kavi/droid/exchange/services/sharedPreferences/SharedPreferenceManager.java
@@ -3,6 +3,9 @@ package com.kavi.droid.exchange.services.sharedPreferences;
 import android.content.Context;
 import android.content.SharedPreferences;
 
+import com.google.gson.Gson;
+import com.kavi.droid.exchange.services.connections.dto.FilterTicketReq;
+
 /**
  * Created by kavi707 on 9/9/17.
  * @author Kavimal Wijewardana <kavi707@gmail.com>
@@ -26,6 +29,8 @@ public class SharedPreferenceManager {
     private static final String LOGGED_USER_IMAGE_URL = "LOGGED_USER_IMAGE_URL";
     private static final String LOGGED_USER_NUMBER = "LOGGED_USER_NUMBER";
     private static final String FCM_PUSH_TOKEN = "FCM_PUSH_TOKEN";
+    private static final String LAST_FILTER_OBJECT = "LAST_FILTER_OBJECT";
+    private static final String IS_TICKET_STATUS_UPDATED = "IS_TICKET_STATUS_UPDATED";
 
     /**
      * Store boolean shared preference value
@@ -79,6 +84,41 @@ public class SharedPreferenceManager {
     private static String readStringSharePreference(Context context, String preferenceKey) {
         SharedPreferences settings = context.getSharedPreferences(EXCHANGE_T_SHARED_PREFERENCES, 0);
         return settings.getString(preferenceKey, "NULL");
+    }
+
+    /**
+     * Store String shared preference value
+     * @param preferenceKey preference key
+     * @param preferenceValue preference value
+     */
+    private static void writeObjectSharePreference(Context context, String preferenceKey, Object preferenceValue) {
+
+        // We need an Editor object to make preference changes.
+        // All objects are from android.context.Context
+        SharedPreferences settings = context.getSharedPreferences(EXCHANGE_T_SHARED_PREFERENCES, 0);
+        SharedPreferences.Editor editor = settings.edit();
+
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(preferenceValue);
+
+        editor.putString(preferenceKey, jsonString);
+
+        // Commit the edits!
+        editor.commit();
+    }
+
+    /**
+     * Get String share preference value
+     * @param preferenceKey preference key
+     * @return String value
+     */
+    private static Object readObjectSharePreference(Context context, String preferenceKey, Class objectClass) {
+        SharedPreferences settings = context.getSharedPreferences(EXCHANGE_T_SHARED_PREFERENCES, 0);
+
+        Gson gson = new Gson();
+        String jsonString = settings.getString(preferenceKey, "NULL");
+
+        return gson.fromJson(jsonString, objectClass);
     }
 
     /**********************************************************************************************/
@@ -187,5 +227,21 @@ public class SharedPreferenceManager {
 
     public static boolean isINeedTypeSelected(Context context){
         return readBooleanSharePreference(context, IS_I_NEED_SELECTED);
+    }
+
+    public static void setLastFilterObject(Context context, FilterTicketReq value) {
+        writeObjectSharePreference(context, LAST_FILTER_OBJECT, value);
+    }
+
+    public static FilterTicketReq getLastFilterObject(Context context){
+        return (FilterTicketReq) readObjectSharePreference(context, LAST_FILTER_OBJECT, FilterTicketReq.class);
+    }
+
+    public static void setIsTicketStatusUpdated(Context context, boolean value){
+        writeBooleanSharePreference(context, IS_TICKET_STATUS_UPDATED, value);
+    }
+
+    public static boolean isTicketStatusUpdated(Context context){
+        return readBooleanSharePreference(context, IS_TICKET_STATUS_UPDATED);
     }
 }
